85021c8e3c90bf7d5d46824d72b87839288130b3
zephyrquest <simone.giamboni929@gmail.com>
2024-10-10 15:18:31 +0200

    made keyboard up to 5 octaves

--- src/main/java/com/github/zephyrquest/modulamusicbox/views/components/Keyboard.java
+++ src/main/java/com/github/zephyrquest/modulamusicbox/views/components/Keyboard.java
@@ -10,24 +10,27 @@ import java.util.Properties;
 import java.util.TreeMap;
 
 public class Keyboard extends StackPane {
-    private final double whiteKeyWidth = 40.;
+    private final double whiteKeyWidth = 30.;
     private final double whiteKeyHeight = 120.;
-    private final double blackKeyWidth = 20.;
+    private final double blackKeyWidth = 15.;
     private final double blackKeyHeight = 60.;
+    private final int octaves = 5;
 
     private final String[] whiteKeyNotes =
             {
+                    "C2", "D2", "E2", "F2", "G2", "A2", "B2",
                     "C3", "D3", "E3", "F3", "G3", "A3", "B3",
                     "C4", "D4", "E4", "F4", "G4", "A4", "B4",
-                    "C5", "D5", "E5", "F5", "G5", "A5", "B5"
-                    //"C6", "D6", "E6", "F6", "G6", "A6", "B6"
+                    "C5", "D5", "E5", "F5", "G5", "A5", "B5",
+                    "C6", "D6", "E6", "F6", "G6", "A6", "B6"
             };
     private final String[] blackKeyNotes =
             {
+                    "CS2", "DS2", "FS2", "GS2", "AS2",
                     "CS3", "DS3", "FS3", "GS3", "AS3",
                     "CS4", "DS4", "FS4", "GS4", "AS4",
-                    "CS5", "DS5", "FS5", "GS5", "AS5"
-                    //"CS6", "DS6", "FS6", "GS6", "AS6"
+                    "CS5", "DS5", "FS5", "GS5", "AS5",
+                    "CS6", "DS6", "FS6", "GS6", "AS6"
             };
     private final Map<Integer, Key> keys;
     private Properties midiNotes;
@@ -54,19 +57,20 @@ public class Keyboard extends StackPane {
         HBox blackKeysContainer = new HBox();
         blackKeysContainer.setSpacing(whiteKeyWidth - blackKeyWidth);
 
-        int octave = 3;
-        for(int o = 0; o < octave; o++) {
+        for(int o = 0; o < octaves; o++) {
             for (int i = 0; i < 5; i++) {
                 BlackKey blackKey = new BlackKey(blackKeyNotes[(o * 5 + i) % blackKeyNotes.length], blackKeyWidth, blackKeyHeight);
 
                 if(i < 2) {
-                    blackKey.setTranslateX(((o * 2 + 1) * whiteKeyWidth - blackKeyWidth / 2) + o * blackKeyWidth / 2 + o * 2);
+                    blackKey.setTranslateX(((o * 2 + 1) * whiteKeyWidth - blackKeyWidth / 2) + o * blackKeyWidth / 2);
                 }
                 else {
-                    blackKey.setTranslateX(((o * 2 + 2) * whiteKeyWidth - blackKeyWidth / 2) + o * blackKeyWidth / 2 + 5 + o * 2);
+                    blackKey.setTranslateX(((o * 2 + 2) * whiteKeyWidth - blackKeyWidth / 2) + o * blackKeyWidth / 2);
                 }
 
                 blackKeys[o * 5 + i] = blackKey;
+                int key = Integer.parseInt(midiNotes.getProperty(blackKey.getNote()));
+                keys.put(key, blackKey);
             }
         }
 

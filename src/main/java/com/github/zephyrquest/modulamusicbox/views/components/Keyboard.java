e8491b2b1326bd04952075e65e67292ccd4a1eda
zephyrquest <simone.giamboni929@gmail.com>
2024-10-10 15:10:38 +0200

    simplefied black keys position

--- src/main/java/com/github/zephyrquest/modulamusicbox/views/components/Keyboard.java
+++ src/main/java/com/github/zephyrquest/modulamusicbox/views/components/Keyboard.java
@@ -20,12 +20,14 @@ public class Keyboard extends StackPane {
                     "C3", "D3", "E3", "F3", "G3", "A3", "B3",
                     "C4", "D4", "E4", "F4", "G4", "A4", "B4",
                     "C5", "D5", "E5", "F5", "G5", "A5", "B5"
+                    //"C6", "D6", "E6", "F6", "G6", "A6", "B6"
             };
     private final String[] blackKeyNotes =
             {
                     "CS3", "DS3", "FS3", "GS3", "AS3",
                     "CS4", "DS4", "FS4", "GS4", "AS4",
                     "CS5", "DS5", "FS5", "GS5", "AS5"
+                    //"CS6", "DS6", "FS6", "GS6", "AS6"
             };
     private final Map<Integer, Key> keys;
     private Properties midiNotes;
@@ -52,31 +54,20 @@ public class Keyboard extends StackPane {
         HBox blackKeysContainer = new HBox();
         blackKeysContainer.setSpacing(whiteKeyWidth - blackKeyWidth);
 
-        for (int i = 0; i < totalBlackKeys; i++) {
-            BlackKey blackKey = new BlackKey(blackKeyNotes[i % blackKeyNotes.length], blackKeyWidth, blackKeyHeight);
+        int octave = 3;
+        for(int o = 0; o < octave; o++) {
+            for (int i = 0; i < 5; i++) {
+                BlackKey blackKey = new BlackKey(blackKeyNotes[(o * 5 + i) % blackKeyNotes.length], blackKeyWidth, blackKeyHeight);
 
-            if(i < 2) {
-                blackKey.setTranslateX(whiteKeyWidth - blackKeyWidth / 2 + 1 + i * 2);
-            }
-            else if(i < 5) {
-                blackKey.setTranslateX(2 * whiteKeyWidth - blackKeyWidth / 2 + 1 + i * 2);
-            }
-            else if(i < 7) {
-                blackKey.setTranslateX(3 * whiteKeyWidth - blackKeyWidth / 2 + 1 + i * 2);
-            }
-            else if(i < 10) {
-                blackKey.setTranslateX(4 * whiteKeyWidth - blackKeyWidth / 2 + 1 + i * 2);
-            }
-            else if(i < 12) {
-                blackKey.setTranslateX(5 * whiteKeyWidth - blackKeyWidth / 2 + 1 + i * 2);
-            }
-            else if(i < 15) {
-                blackKey.setTranslateX(6 * whiteKeyWidth - blackKeyWidth / 2 + 1 + i * 2);
-            }
+                if(i < 2) {
+                    blackKey.setTranslateX(((o * 2 + 1) * whiteKeyWidth - blackKeyWidth / 2) + o * blackKeyWidth / 2 + o * 2);
+                }
+                else {
+                    blackKey.setTranslateX(((o * 2 + 2) * whiteKeyWidth - blackKeyWidth / 2) + o * blackKeyWidth / 2 + 5 + o * 2);
+                }
 
-            blackKeys[i] = blackKey;
-            int key = Integer.parseInt(midiNotes.getProperty(blackKey.getNote()));
-            keys.put(key, blackKey);
+                blackKeys[o * 5 + i] = blackKey;
+            }
         }
 
         whiteKeysContainer.getChildren().addAll(whiteKeys);
